916355dc9fe463e426a5ce2a07b8f72ef8181753
Nachtalb <na@nachtalb.io>
2024-01-28 18:51:13 +0100

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use aer::{args::Args, routes::route_config};
 use clap::Parser;
 use std::sync::Arc;
@@ -7,11 +7,14 @@ use std::sync::Arc;
 async fn main() -> std::io::Result<()> {
     let args: Args = Args::parse();
 
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
     let shared_args = Arc::new(args);
 
     println!("Starting server at http://localhost:9999");
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .app_data(web::Data::new(shared_args.clone()))
             .configure(route_config)
     })
